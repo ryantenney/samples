e38cb218caacdecbefaa4455a4eabec2b30dee13
Ryan Tenney <ryan@10e.us>
2013-12-17 00:33:10 -0500

    Blink green LED to indicate charge state

--- hexbright4.ino
+++ hexbright4.ino
@@ -109,9 +109,22 @@ void loop()
   static byte blink;
   unsigned long time = millis();
   
-  // Blink the indicator LED now and then
-  digitalWrite(DPIN_GLED, (time&0x03FF)?LOW:HIGH);
-
+  // Check the state of the charge controller
+  int chargeState = analogRead(APIN_CHARGE);
+  if (chargeState < 128) // Low - charging
+  {
+    digitalWrite(DPIN_GLED, (time&0x0100)?LOW:HIGH);
+  }
+  else if (chargeState > 768) // High - charged
+  {
+    digitalWrite(DPIN_GLED, HIGH);
+  }
+  else // Hi-Z - shutdown
+  {
+    // Blink the indicator LED now and then
+    digitalWrite(DPIN_GLED, (time&0x03FF)?LOW:HIGH);
+  }
+  
   // Check the serial port
   if (Serial.available())
   {
